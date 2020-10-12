91929fe1c37407f65e8583a88d33a46812394f4d
Austin-Derrick <aderrickson@colum.edu>
2020-10-12 08:36:41 -0500

    Added a second trigger, and refeactored code to use an Action and parameters.

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -28,9 +28,9 @@ public class ContextualMessageController : MonoBehaviour
         canvasGroup.alpha = 0;
     }
 
-    private void OnContextualMessageTriggered()
+    private void OnContextualMessageTriggered(string message, float messageDuration)
     {
-        StartCoroutine(ShowMessage("Testing", 5));
+        StartCoroutine(ShowMessage(message, messageDuration));
     }
 
     private void OnEnable()
