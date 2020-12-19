8d2be930597d746aef64dfd33d44c4c2072e3e0d
Austin-Derrick <aderrickson@colum.edu>
2020-12-18 22:49:36 -0600

    Included input system in pre-existing scripts

---
+++ Assets/Scripts/CameraInputHandler.cs
@@ -0,0 +1,22 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraInputHandler : MonoBehaviour
+{
+    private CinemachineFreeLook freeLookCamera;
+
+    private void Start()
+    {
+        freeLookCamera = GetComponent<CinemachineFreeLook>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        var pointerDelta = context.ReadValue<Vector2>();
+        freeLookCamera.m_XAxis.Value += pointerDelta.x;
+        freeLookCamera.m_YAxis.Value += pointerDelta.y;
+    }
+}
